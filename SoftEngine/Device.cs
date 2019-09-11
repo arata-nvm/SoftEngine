f533cf307eca4a236fc291f27a0a64554187abad
Arata <arata04962@gmail.com>
2019-09-11 11:32:22 +0900

    Part 1

---
+++ SoftEngine/Device.cs
@@ -0,0 +1,80 @@
+﻿using System.Windows;
+using System.Windows.Media.Imaging;
+using SharpDX;
+
+namespace SoftEngine
+{
+    public class Device
+    {
+        private byte[] backBuffer;
+        private WriteableBitmap bmp;
+
+        public Device(WriteableBitmap bmp)
+        {
+            this.bmp = bmp;
+            backBuffer = new byte[bmp.PixelWidth * bmp.PixelHeight * 4];
+        }
+
+        public void Clear(byte r, byte g, byte b, byte a)
+        {
+            for (var index = 0; index < backBuffer.Length; index += 4)
+            {
+                backBuffer[index] = b;
+                backBuffer[index + 1] = g;
+                backBuffer[index + 2] = r;
+                backBuffer[index + 3] = a;
+            }
+        }
+
+        public void Present()
+        {
+            var rect = new Int32Rect(0, 0, bmp.PixelWidth, bmp.PixelHeight);
+            bmp.WritePixels(rect, backBuffer, bmp.PixelWidth * 4, 0, 0);
+        }
+
+        public void PutPixel(int x, int y, Color4 color)
+        {
+            var index = (x + y * bmp.PixelWidth) * 4;
+            backBuffer[index] = (byte) (color.Blue * 255);
+            backBuffer[index + 1] = (byte) (color.Green * 255);
+            backBuffer[index + 2] = (byte) (color.Red * 255);
+            backBuffer[index + 3] = (byte) (color.Alpha * 255);
+        }
+
+        public Vector2 Project(Vector3 coord, Matrix transMat)
+        {
+            var point = Vector3.TransformCoordinate(coord, transMat);
+            var x = point.X * bmp.PixelWidth + bmp.PixelWidth / 2.0f;
+            var y = -point.Y * bmp.PixelHeight + bmp.PixelHeight / 2.0f;
+            return new Vector2(x, y);
+        }
+
+        public void DrawPoint(Vector2 point)
+        {
+            if (point.X >= 0 && point.Y >= 0 && point.X < bmp.PixelWidth && point.Y < bmp.PixelHeight)
+            {
+                PutPixel((int) point.X, (int) point.Y, new Color4(1.0f, 1.0f, 1.0f, 1.0f));
+            }
+        }
+
+        public void Render(Camera camera, params Mesh[] meshes)
+        {
+            var viewMatrix = Matrix.LookAtLH(camera.Position, camera.Target, Vector3.UnitY);
+            var projectionMatrix =
+                Matrix.PerspectiveFovRH(0.78f, (float) bmp.PixelWidth / bmp.PixelHeight, 0.01f, 1.0f);
+
+            foreach (var mesh in meshes)
+            {
+                var worldMatrix = Matrix.RotationYawPitchRoll(mesh.Rotation.Y, mesh.Rotation.X, mesh.Rotation.Z) *
+                                  Matrix.Translation(mesh.Position);
+                var transformMatrix = worldMatrix * viewMatrix * projectionMatrix;
+
+                foreach (var vertex in mesh.Vertices)
+                {
+                    var point = Project(vertex, transformMatrix);
+                    DrawPoint(point);
+                }
+            }
+        }
+    }
+}
---
+++ SoftEngine/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using SharpDX;
+
+namespace SoftEngine
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+
+        public WriteableBitmap Bmp { get; set; }
+
+        private Device device;
+        Mesh mesh = new Mesh("Cube", 8);
+        Camera camera = new Camera();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        private void Init()
+        {
+            Bmp = new WriteableBitmap(640, 480, 96, 96, PixelFormats.Pbgra32, null);
+            device = new Device(Bmp);
+            
+            Image image = new Image();
+            image.Source = Bmp;
+            XCanvas.Children.Add(image);
+            
+            mesh.Vertices[0] = new Vector3(-1, 1, 1);
+            mesh.Vertices[1] = new Vector3(1, 1, 1);
+            mesh.Vertices[2] = new Vector3(-1, -1, 1);
+            mesh.Vertices[3] = new Vector3(-1, -1, -1);
+            mesh.Vertices[4] = new Vector3(-1, 1, -1);
+            mesh.Vertices[5] = new Vector3(1, 1, -1);
+            mesh.Vertices[6] = new Vector3(1, -1, 1);
+            mesh.Vertices[7] = new Vector3(1, -1, -1);
+            
+            camera.Position = new Vector3(0, 0, 10.0f);
+            camera.Target = Vector3.Zero;
+
+            CompositionTarget.Rendering += Rendering;
+        }
+
+        void Rendering(object sender, object e)
+        {
+            device.Clear(0, 0, 0, 255);
+            mesh.Rotation = new Vector3(mesh.Rotation.X + 0.01f, mesh.Rotation.Y + 0.01f, mesh.Rotation.Z + 0.01f);
+
+            device.Render(camera, mesh);
+            device.Present();
+        }
+    }
+}
